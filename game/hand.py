a18c5f18ddfe0e204e80a0c93793ae2277e6b8ed
razak17 <moabdulrazak17@gmail.com>
2020-06-02 11:06:12 +0000

    Full House Implementation

--- game/hand.py
+++ game/hand.py
@@ -7,6 +7,7 @@ class Hand():
     @property
     def _rank_validations_from_best_to_worst(self):
         return (
+            ("Full House", self._full_house),
             ("Flush", self._flush),
             ("Straight", self._straight),
             ("Three of a Kind", self._three_of_a_kind),
@@ -14,6 +15,9 @@ class Hand():
             ("Pair", self._pair),
             ("High Card", self._high_card)
         )
+
+    def _full_house(self):
+        return self._three_of_a_kind() and self._pair()
 
     def best_rank(self):
         for rank in self._rank_validations_from_best_to_worst:
