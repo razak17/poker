8ac046743f74355f11a626065490c6eea773ea72
razak17 <moabdulrazak17@gmail.com>
2020-06-05 16:25:34 +0000

    Remove cards from deck

--- tests/test_game_round.py
+++ tests/test_game_round.py
@@ -1,7 +1,8 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 
 from game.game_round import GameRound
+from game.card import Card
 
 class GameROundTest(unittest.TestCase):
     def test_stores_deck_and_players(self):
@@ -43,3 +44,35 @@ class GameROundTest(unittest.TestCase):
         game_round.play()
 
         mock_deck.shuffle.assert_called_once()
+
+    def test_deals_two_initial_cards_from_deck_to_each_player(self):
+        first_two_cards = [
+            Card(rank="2", suit="Hearts"),
+            Card(rank="6", suit="Clubs")
+        ]
+
+        next_two_cards = [
+            Card(rank="9", suit="Diamonds"),
+            Card(rank="4", suit="Spades")
+        ]
+
+        mock_deck = MagicMock()
+        mock_deck.remove_cards.side_effect = [first_two_cards, next_two_cards]
+
+        mock_player1 = MagicMock()
+        mock_player2 = MagicMock()
+        players = [mock_player1, mock_player2]
+
+        game_round = GameRound(
+            deck=mock_deck,
+            players=players
+        )
+
+        game_round.play()
+
+        mock_deck.remove_cards.assert_has_calls([
+            call(2), call(2)
+        ])
+
+        # mock_player1.add_cards.assert_called_with(first_two_cards)
+        # mock_player2.add_cards.assert_called_with(next_two_cards)
